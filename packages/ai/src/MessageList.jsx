a68f5b2c34a533264f33a5c5a5e23d1a4b7f5b63
Yuqian Ma <mayq0422@gmail.com>
2023-06-19 02:30:29 +0800

    feat: replay message

--- packages/ai/src/MessageList.jsx
+++ packages/ai/src/MessageList.jsx
@@ -1,32 +1,40 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { observer } from "mobx-react-lite";
+import { StoreContext } from "./store";
 import { UserMessage } from "./components/UserMessage";
 import { AIMessage } from "./components/AIMessage";
 
-const TestString = "```javascript\nimport * as THREE from 'three';\n\n// Create scene\nconst scene = new THREE.Scene();\n\n// Create camera\nconst camera = new THREE.PerspectiveCamera(\n  75,\n  window.innerWidth / window.innerHeight,\n  0.1,\n  1000\n);\ncamera.position.z = 5;\n\n// Create renderer\nconst renderer = new THREE.WebGLRenderer();\nrenderer.setSize(window.innerWidth, window.innerHeight);\ndocument.body.appendChild(renderer.domElement);\n\n// Create geometry\nconst geometry = new THREE.BoxGeometry();\nconst material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });\nconst cube = new THREE.Mesh(geometry, material);\nscene.add(cube);\n\n// Create controls\nconst controls = new THREE.OrbitControls(camera, renderer.domElement);\n\n// Create directional light\nconst directionalLight = new THREE.DirectionalLight(0xffffff, 1);\ndirectionalLight.position.set(1, 1, 1).normalize();\nscene.add(directionalLight);\n\n// Create game loop\nfunction animate() {\n  requestAnimationFrame(animate);\n\n  // Rotate cube\n  cube.rotation.x += 0.01;\n  cube.rotation.y += 0.01;\n\n  // Update controls\n  controls.update();\n\n  renderer.render(scene, camera);\n}\nanimate();\n``` \n\nIn this updated version, I added the following features:\n\n- Added controls using `OrbitControls` to allow the player to move the camera around the scene.\n- Created a directional light to provide better illumination on the objects in the scene.\n\nFeel free to continue adding more features and functionality to this game using the THREE.js library!";
 
-export const MessageList = () => {
-	const [message, setMessage] = useState("");
+export const MessageList = observer(() => {
+	// const [message, setMessage] = useState("");
 
-	useEffect(() => {
-		let index = 0;
-		const intervalId = setInterval(() => {
-			index++;
-			setMessage(TestString.substring(0, index));
-			if (index >= TestString.length) {
-				clearInterval(intervalId);
-			}
-			document.querySelector('#message-list-bottom').scrollIntoView();
-		}, 16);
+	// useEffect(() => {
+	// 	let index = 0;
+	// 	const intervalId = setInterval(() => {
+	// 		index++;
+	// 		setMessage(TestString.substring(0, index));
+	// 		if (index >= TestString.length) {
+	// 			clearInterval(intervalId);
+	// 		}
+	// 		document.querySelector('#message-list-bottom').scrollIntoView();
+	// 	}, 16);
 
-		return () => {
-			clearInterval(intervalId);
-		}
-	}, []);
+	// 	return () => {
+	// 		clearInterval(intervalId);
+	// 	}
+	// }, []);
+
+	const store = useContext(StoreContext);
+	const messages = store.messages;
 
 	return (<>
-		<UserMessage message="foo" />
-		<AIMessage message="bar" />
-		<UserMessage message="1" />
-		<AIMessage message={message} />
+		{ messages.map((message, index) => {
+			if (message.role === "user") {
+				return <UserMessage key={index} message={message.content} />
+			} else if (message.role === "assistant") {
+				return <AIMessage key={index} message={message.content} />
+			}
+		}) }
 	</>);
-};
+});
---
+++ packages/ai/src/store/index.js
@@ -0,0 +1,86 @@
+import { createContext } from 'react';
+import { makeAutoObservable } from "mobx";
+import MESSAGES from "./message.json";
+
+const MESSAGE_STREAM_STEP = 10;
+
+function scrollLatestIntoView() {
+	document.querySelector('#message-list-bottom').scrollIntoView();
+}
+
+class Store {
+	messages = MESSAGES.slice(0, 2);
+	isRunning = false;
+
+	_intervalId = null;
+
+	constructor() {
+		makeAutoObservable(this);
+
+		// for debug
+		document.addEventListener("keydown", (e) => {
+			// console.log(e);
+			if (e.key === "1") {
+				this.start();
+			}
+		});
+
+		document.addEventListener("keyup", (e) => {
+			if (e.key === "1") {
+				this.stop();
+			}
+		});
+	}
+
+	_resetMessages() {
+		this.messages = MESSAGES.slice(0, 2);
+		scrollLatestIntoView();
+	}
+
+	start() {
+		if (this.isRunning) {
+			return;
+		}
+		this.isRunning = true;
+
+		if (this.messages.length === MESSAGES.length) {
+			this._resetMessages();
+		}
+
+		// next message's role is user
+		const nextMessageIndex = this.messages.length;
+		this.messages.push(MESSAGES[nextMessageIndex]);
+		const assistantMessage = MESSAGES[nextMessageIndex + 1];
+
+		// prepare next assistant message
+		this.messages.push({
+			role: "assistant",
+			content: "",
+		});
+
+		let stringIndex = 0;
+		this._intervalId = setInterval(() => {
+			const message = this.messages.at(-1);
+			message.content += assistantMessage.content.slice(stringIndex, stringIndex + MESSAGE_STREAM_STEP);
+			stringIndex += MESSAGE_STREAM_STEP;
+			if (stringIndex >= assistantMessage.content.length) {
+				this.stop();
+			}
+			scrollLatestIntoView();
+		}, 16);
+	}
+
+	stop() {
+		if (!this.isRunning) {
+			return;
+		}
+		this.isRunning = false;
+		clearInterval(this._intervalId);
+		this._intervalId = null;
+	}
+
+}
+
+const store = new Store()
+
+export const StoreContext = createContext(store);
